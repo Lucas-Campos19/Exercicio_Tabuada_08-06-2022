64fc7064f10d81902e4538b2df454115c36c512d
lucas <lucas@LAPTOP-PAQTMCQ7>
2022-06-14 16:36:02 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio_Tabuada_08-06-2022/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Tabuada_08_06_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num, num1, num2;
+
+            //Console.Write(" Digite a tabuada escolhida: ");
+            //num = int.Parse(Console.ReadLine());
+            
+            //Console.Write(" Digite o número incial escolhido: ");
+            //num1 = int.Parse(Console.ReadLine());
+            
+            //Console.Write(" Digite o número final escolhido: ");
+            //num2 = int.Parse(Console.ReadLine());
+
+            //for(; num1 <= num2; num1++)
+            //{
+            //    Console.WriteLine(num + " X " + num1 + " = " + num * num1);
+            //}
+
+            //while (num1 <= num2)
+            //{
+            //    Console.WriteLine(num + " X " + num1 + " = " + num * num1);
+            //    num1++;
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine(num + " X " + num1 + " = " + num * num1);
+            //    num1++;
+
+            //} while (num1 <= num2);
+            //Console.ReadKey();
+        }
+    }
+}
